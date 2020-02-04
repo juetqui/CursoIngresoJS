ab2f1b8ddf2f525b45ca97086e1b6c279ac9d981
juetqui <60628743+juetqui@users.noreply.github.com>
2020-02-04 19:07:00 -0300

    primer commit
    primer commit

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,8 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+function mostrar()
+{
+	//var nombre;
+	//nombre = prompt ("Escriba su nombre", "N N");
+	//alert(nombre);
+	alert ("Esto funciona de maravilla");	
+}
