cd2c41c45a0151f749df2ebf64855abfd6cddf2d
juetqui <60628743+juetqui@users.noreply.github.com>
2020-02-05 05:07:48 -0300

    Practica ej 10 listo
    Segundo ejercicio con porcentaje

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe;
+	var resultado;
+	var porcentaje;
+
+	importe=document.getElementById('importe').value;
+	importe=parseInt(importe);
+
+	porcentaje=0.75;
+
+	resultado=importe*porcentaje;
+
+	document.getElementById('resultado').value=resultado;
 }
