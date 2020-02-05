152ca2f39f23a05770e6821f38759672b2f10bd8
juetqui <60628743+juetqui@users.noreply.github.com>
2020-02-05 04:17:47 -0300

    practica ej 1 listo
    primer uso de alert

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,8 +1,5 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	//var nombre;
-	//nombre = prompt ("Escriba su nombre", "N N");
-	//alert(nombre);
-	alert ("Esto funciona de maravilla");	
+	alert ("Esto funciona de maravilla")
 }
