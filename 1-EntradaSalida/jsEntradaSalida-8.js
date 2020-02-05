821a687b384b188f185dd4746b648a3f80caf048
juetqui <60628743+juetqui@users.noreply.github.com>
2020-02-05 04:57:55 -0300

    Practica ej 8 listo
    Primer uso del resto.

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,17 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var dividendo;
+	var divisor;
+	var	operacion;
+
+	dividendo=document.getElementById('numeroDividendo').value;
+	dividendo=parseInt(dividendo);
+
+	divisor=document.getElementById('numeroDivisor').value;
+	divisor=parseInt(divisor);
+
+	operacion=dividendo%divisor;
+
+	alert ("El resto es "+operacion+" .");
 }
