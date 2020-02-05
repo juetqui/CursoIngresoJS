b2a8a37f61d9cc0d41eddd50440947b7bbeec194
juetqui <60628743+juetqui@users.noreply.github.com>
2020-02-05 05:03:30 -0300

    Practica ej 9 listo
    Primer uso de porcentajes.

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,17 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var importe;
+	var resultado;
+	var	porcentaje;
 
+	importe=document.getElementById('sueldo').value;
+	importe=parseInt(importe);
+
+	porcentaje=1.1;
+
+	resultado=importe*porcentaje;
+
+	document.getElementById('resultado').value=resultado;
 	
 }
