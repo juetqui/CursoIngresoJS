c2dd8b0243c06e03219211ce9dabb03d96ca7325
juetqui <60628743+juetqui@users.noreply.github.com>
2020-02-05 04:52:32 -0300

    Practica ej 7 listo
    Ejercicio con todas las operaciones.

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,69 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primerNumero;
+	var segundoNumero;
+	var	operacion;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
+
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+
+	operacion=primerNumero+segundoNumero;
+
+	alert ("la Suma es "+operacion);
 }
 
 function restar()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var	operacion;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
+
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+
+	operacion=primerNumero-segundoNumero;
+
+	alert ("la Resta es "+operacion);
 }
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var	operacion;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
+
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+
+	operacion=primerNumero*segundoNumero;
+
+	alert ("la Multiplicación es "+operacion);
 }
 
 function dividir()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var	operacion;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
+
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+
+	operacion=primerNumero/segundoNumero;
+
+	alert ("El resultado es "+operacion);
 }
 
