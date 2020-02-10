a9fc5b77153b3b7e5e169018028ddb1847272043
juetqui <60628743+juetqui@users.noreply.github.com>
2020-02-10 20:36:17 -0300

    practica if 3 listo

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
 
+	edad=document.getElementById('edad').value;
+
+	if (edad>17) 
+	{
+		alert ("si, es mayor de edad.");
+	}
+	if (edad<18)
+	{
+		alert ("si, es menor de edad.");
+	}
 
 }//FIN DE LA FUNCIÓN
