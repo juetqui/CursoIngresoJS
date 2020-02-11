c25eaa02b04a2730919525cef8350f00fd97c340
juetqui <60628743+juetqui@users.noreply.github.com>
2020-02-10 22:05:47 -0300

    practica if 7 listo

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,7 +1,27 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
+	var estadoCivil;
 
+	edad=document.getElementById('edad').value;
+	estadoCivil=document.getElementById('estadoCivil').value;
+
+	if (edad<18) 
+	{
+		if (estadoCivil!="Soltero") 
+			{
+				alert("Es muy pequeño para NO ser soltero.");
+			}
+		else
+			{
+				alert("Esta bien.");
+			}
+	else
+	{
+		alert("Esta bien.");
+	}			
+	}
 	
 
 
